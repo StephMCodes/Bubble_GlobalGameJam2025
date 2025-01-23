135608b61af730dc5a972bfc1946fed60336ceea
Bella P. <bellaperezppg@gmail.com>
2025-01-23 13:03:08 -0500

    Added Text mesh pro started Main Menu

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene("Chapter 1");
+    }
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
