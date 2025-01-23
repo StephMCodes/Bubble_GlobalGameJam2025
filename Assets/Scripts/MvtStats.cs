f29ad5ce1e364affc2a4013ca3bb51c3a85b3c79
Stephanie M <shelovesgames965@gmail.com>
2025-01-22 21:55:08 -0500

    scriptable object MvtStats iteration 1
    to adjust acceleration/decelaration and speed and has raycast for ground or overhead detection

--- Assets/Scripts/MvtStats.cs
+++ Assets/Scripts/MvtStats.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MvtStats : MonoBehaviour
+[ CreateAssetMenu(menuName = "Player Movement")]
+public class MvtStats : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //header for editor
+    [Header("Walk")]
+    //range allows to adjust value of var in editor
+    [Range(1f, 100f)] public float MaxWalkSpeed = 12.5f;
+    [Range(0.25f, 50f)] public float GroundAcceleration = 5f;
+    [Range(0.25f, 50f)] public float GroundDeceleration = 20f;
+    [Range(0.25f, 50f)] public float AirAcceleration = 5f;
+    [Range(0.25f, 50f)] public float AirDeceleration = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Run")]
+    [Range(1f, 100f)] public float MaxRunSpeed = 20f;
+
+    [Header("Ground/Collision Checking")]
+    //knowing whether we are on the ground or not lets us choose between
+    //applying ground or air accelaration/decelaration
+    public LayerMask GroundLayer;
+    public float GroundDetectionRaycastLength = 0.02f;
+    public float HeadDetectionRaycastLength = 0.02f;
+    [Range(0f, 1f)] public float HeadWidth = 0.75f;
+
 }
