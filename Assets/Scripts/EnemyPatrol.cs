a459b5a36852fc8eb057d705ac6a0f3f172d1bef
Stephanie M <shelovesgames965@gmail.com>
2025-01-24 19:33:54 -0500

    patrol iteration 2

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -8,15 +8,71 @@ public class EnemyPatrol : MonoBehaviour
     [SerializeField] private Transform _rightEdge;
     [SerializeField] private Transform _enemy;
     [SerializeField] private float _speed;
+    private bool _movingLeft;
+    [SerializeField] private float _idleDuration;
+    private float _idleTimer;
 
 
+    private Vector3 initScale; //initial scale
+
+    // [SerializeField] private animator anim
+
+    private void Awake()
+    {
+        initScale = _enemy.localScale;
+    }
+
+    private void Update()
+    {
+        if (_movingLeft)
+        {
+            if (_enemy.position.x >= _leftEdge.position.x)
+            {
+                MoveTowards(-1); //LEFT
+            }
+            else //change direction now
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if (_enemy.position.x <= _rightEdge.position.x)
+            {
+                MoveTowards(1); //RIGHT
+            }
+            else
+            {
+                DirectionChange();
+            }
+
+        }
+    }
 
     private void MoveTowards(int _direction)
     {
-        //make enemy face correctly
+        //begin walk anim
+        //Animation.SetBool("move", true);
 
+        //reset idle timer
+        _idleTimer = 0;
+
+        //make enemy face correctly
+        _enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction, initScale.y, initScale.z);
         //move
         _enemy.position = new Vector3(_enemy.position.x + Time.deltaTime * _direction * _speed, _enemy.position.y, _enemy.position.z);
+
+    }
+
+    private void DirectionChange()
+    {
+        //stop walk anim
+        //Animation.SetBool("move", false);
+        _idleTimer += Time.deltaTime;
+        if (_idleTimer > _idleDuration)
+        {
+            _movingLeft = !_movingLeft; //swap moving direction false is right
+        }
     }
 
 }
