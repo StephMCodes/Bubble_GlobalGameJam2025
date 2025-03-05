60b534869502d46d7b20f7ec168587ef3f2ce925
Bella P. <bellaperezppg@gmail.com>
2025-03-04 22:59:29 -0500

    Working on pause menu ch1

---
+++ Assets/PauseMenuControls.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuControls : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturntoMainMenu()
+    {
+
+    }
+
+}
