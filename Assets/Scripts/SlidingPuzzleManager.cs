31da751d27071fa6e27413ecc999db1e88073559
Stephanie M <shelovesgames965@gmail.com>
2025-03-07 10:27:59 -0500

    it shuffles and is interactable! had to set inversion
    100 seems fair and is even

--- Assets/Scripts/SlidingPuzzleManager.cs
+++ Assets/Scripts/SlidingPuzzleManager.cs
@@ -9,6 +9,9 @@ public class SlidingPuzzleManager : MonoBehaviour
     //reference game piece
     [SerializeField] private Transform piecePrefab;
 
+    //keeping track of where they are to track them around
+    private List<Transform> pieces;
+
     //the last piece needs to be empty/non present for the puzzle, record it here
     private int emptyLocation;
 
@@ -19,8 +22,8 @@ public class SlidingPuzzleManager : MonoBehaviour
     private void CreateGamePieces(float gapThickness)
     {
         //width of each tile
-        float width = 1/(float)size; //so each piece will be one third of a unit
-            
+        float width = 1 / (float)size; //so each piece will be one third of a unit
+
         //to iterate over the size x size 2D array we create a grid
         for (int row = 0; row < size; row++) //go through each row
         {
@@ -28,10 +31,12 @@ public class SlidingPuzzleManager : MonoBehaviour
             {
                 //create the pieces in center position
                 Transform piece = Instantiate(piecePrefab, gameTransform);
+                //add to the list to identify what piece we clicked
+                pieces.Add(piece);
                 //place the piece with center being the anchor
                 piece.localPosition = new Vector3(-1 + (2 * width * col) + width, +1 - (2 * width * row) - width, 0);
                 //scale the piece, substracting the gap to have space between pieces
-                piece.localScale = ((2 *width) - gapThickness) * Vector3.one;
+                piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
                 //name the piece so we can check if theyre all aligned later
                 piece.name = $"{(row * size) + col}";
                 //empty space bottom right
@@ -58,17 +63,112 @@ public class SlidingPuzzleManager : MonoBehaviour
 
         }
     }
+
+    private bool SwapIfValid(int i, int offset, int colCheck)
+    {
+        //when on rightmost column we dont wanna look left and vice versa
+        //because on the screen there would be the edge of the puzzle
+        //but in the list it would be the leftmost one up a row
+        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        {
+            //valid, swap pieces in game state
+            (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
+            //swap transforms
+            (pieces[i].localPosition, pieces[i + offset].localPosition) = (pieces[i + offset].localPosition, pieces[i].localPosition);
+            //update empty location
+            emptyLocation = i;
+            return true;
+        }
+        return false;
+    }
     // Start is called before the first frame update
+    private void Shuffle()
+    {
+        int count = 0;
+        int last = 0;
+        int inversionsLeft = (100); //it needs to be even to be solvable
+        while (inversionsLeft != 0)
+        {
+            // Pick a random location.
+            int rnd = Random.Range(0, size * size);
+            // Only thing we forbid is undoing the last move.
+            if (rnd == last) { continue; }
+            last = emptyLocation;
+            // Try surrounding spaces looking for valid move.
+            if (SwapIfValid(rnd, -size, size))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, +size, size))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, -1, 0))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, +1, size - 1))
+            {
+                count++;
+            }
+            inversionsLeft--;
+        }
+    }
+    // We name the pieces in order so we can use this to check completion.
+    private bool CheckCompletion()
+    {
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            if (pieces[i].name != $"{i}")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     void Start()
     {
+        //init. list
+        pieces = new List<Transform>();
         //size x size grid
         size = 3;
         CreateGamePieces(0.01f); //the parameter is thickness between pieces
+        //shuffle
+        Shuffle();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //check
+        if (CheckCompletion())
+        {
+            Debug.Log("DONE");
+        }
+        //time to detect mouse clicks!
+        //on click send out ray to see if we clicked a piece
+        if (Input.GetMouseButtonDown(0)) //zero indicates left mouse click
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                //Debug.Log("TRUE");
+                //iterate over array to see which one we hit
+                for (int i = 0; i < pieces.Count; i++)
+                {
+                    if (pieces[i] == hit.transform)
+                    {
+                        //check every direction around it to see if valid move
+                        //break out on success if not keep trying
+                        if (SwapIfValid(i, -size, size)) { break; }
+                        if (SwapIfValid(i, +size, size)) { break; }
+                        if (SwapIfValid(i, -1, 0)) { break; }
+                        if (SwapIfValid(i, +1, size - 1)) { break; }
+                    }
+                }
+
+            }
+
+        }
     }
 }
